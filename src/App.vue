ea583a6d313acf08f443b315fd5db312fff05be0
pimrampha <pimrampha.m@gmail.com>
2019-04-29 00:11:45 +0700

    Just show TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TodoList test="อะไรวะ"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoList
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Todolist</h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, quas! Inventore ut porro ratione temporibus dolores numquam voluptatibus pariatur quod doloribus officia, iste commodi distinctio recusandae eum veniam non ipsam.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+      test: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
